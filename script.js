0e42e743c60817e1ee1374f0609e1a923d6a6f56
devPadua <igortapadua@gmail.com>
2024-05-28 16:00:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function gerar(){
+var txtn = document.getElementById('numero')
+var n = Number(txtn.value)
+var res = document.getElementById('res')
+var c = 1
+var multitxt = document.getElementById('multi')
+var multi = Number(multitxt.value)
+res.innerHTML = `Tabuada do ${n}: <br>`
+    while (c <= multi){
+        var r =  c * n
+        res.innerHTML += `<br> ${n} x ${c} = ${r} <br>`
+        var c = c + 1
+}
+
+}
