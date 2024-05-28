235c35a68237306bbd420af099ea49d305324027
devPadua <igortapadua@gmail.com>
2024-05-28 16:08:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var res = document.getElementById('res')
 var c = 1
 var multitxt = document.getElementById('multi')
 var multi = Number(multitxt.value)
-res.innerHTML = `Tabuada do ${n}: <br>`
+res.innerHTML = `Tabuada do ${n} até o ${multi}: <br>`
     while (c <= multi){
         var r =  c * n
         res.innerHTML += `<br> ${n} x ${c} = ${r} <br>`
